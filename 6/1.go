a0a6925a1eee7123f700eef09e1b124f922a9be2
Reed Allman <reed@rdallman.com>
2022-12-23 17:14:40 -0800

    day 6

---
+++ 6/1.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	readFile, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanRunes)
+
+	buf := make([]string, 4)
+
+	var i int
+	for i = 0; fileScanner.Scan(); i++ {
+		char := fileScanner.Text()
+
+		if i < 4 {
+			buf[i] = char
+			if i < 3 {
+				continue
+			}
+		} else {
+			buf = buf[1:]
+			buf = append(buf, char)
+		}
+
+		var repeat bool
+		for j, b := range buf {
+			// lazy n^2 check membership on every char
+			for k := j + 1; k < len(buf); k++ {
+				bb := buf[k]
+				repeat = repeat || (bb == b && j != k)
+			}
+		}
+
+		if !repeat {
+			break
+		}
+	}
+
+	readFile.Close()
+	fmt.Println(i + 1)
+}
